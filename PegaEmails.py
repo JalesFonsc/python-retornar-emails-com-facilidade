d888829b4236df639d83cb3b3b1d525ee323488f
Jales Fonseca <jalesfonsecaneto@gmail.com>
2024-09-24 20:46:08 -0300

    Adicionado comentário

--- PegaEmails.py
+++ PegaEmails.py
@@ -1,15 +1,15 @@
 import re
 
 # Caso queira mudar os delimitadores, basta alterar o regex. Por padrão, deixei os delimitadres < >.
-# Passa a string de e-mails no segundo parâmetro;
+# Passa a string de e-mails no segundo parâmetro.
 emails = re.findall('\<(.*?)\>', 'SUBSTITUA O TEXTO/EMAIL AQUI')
 
-# Conjunto joker para guardar os email-s sem repetição
+# Conjunto para guardar os email-s removendo as repetições caso haja.
 emails_filtrados = set(emails)
 
-# Verifica se não encontrou e-mails
+# Verifica se não encontrou e-mails.
 if not emails_filtrados:
     print('Nenhum e-mail foi encontrado.')
 else:
-    # Imprime os e-mails filtrados, separados por vírgula
+    # Imprime os e-mails filtrados, separados por vírgula.
     print(', '.join(emails_filtrados))
